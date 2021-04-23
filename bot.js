85cde9a0bc780d8e2737c3495def2ef15211ffb0
Melvin Carvalho <melvincarvalho@gmail.com>
2021-04-23 22:22:22 +0200

    mark as a function

--- bot.js
+++ bot.js
@@ -19,6 +19,7 @@ commands.balances = require('./commands/balances.js').balances
 commands.wallet = require('./commands/wallet.js').wallet
 commands.givers = require('./commands/givers.js').givers
 commands.balance = require('./commands/balance.js').balance
+commands.mark = require('./commands/mark.js').mark
 
 // functions
 const getNickFromId = require('./functions.js').getNickFromId
@@ -184,69 +185,6 @@ console.log(tx[0] , tx[0].length === 64 , tx[0].match(/^[0-9]abcdef$/))
     // ctx.reply(`sweep ${message[1]}`)
   }
 
-  // mark
-  if (message[0].toLocaleLowerCase() === 'mark') {
-    console.log('mark', message)
-
-    // get user for mark
-    var user = from
-
-    // get destination
-    var destination = null
-    if (message[1] && usernames[message[1]]) {
-      destination = usernames[message[1]]
-    }    
-    if (!destination) {
-      ctx.reply('no destination to mark')
-      return;
-    }
-
-    // get amount
-    var amount = null
-    if (message[2]) {
-      var amount = parseInt(message[2])
-    }
-    if (!amount) {
-      ctx.reply('specify a non zero amount to mark')
-      return;
-    }
-
-    // get reason
-    message.shift()
-    message.shift()
-    message.shift()
-    console.log('reason', message)
-
-    // get balance
-    var balance = ledger[user] || 0
-    console.log(ledger)
-    console.log(balance)
-
-    // check funds
-    if (balance && balance > 0 && amount && amount > 0 && balance - amount >= 0) {
-      console.log('marking')
-      ledger[user] -= amount
-      ledger[destination] += amount
-      console.log('newledger', ledger)
-      ctx.reply('marked ' + amount + ' to ' + destination)
-      var credit = { source: user, destination: destination, amount: amount, comment: message.join(' '), timestamp: Math.floor(Date.now() / 1000) }
-      console.log(credit)
-      if (credit) {
-        credits.push(credit)
-      }
-      console.log(credits)
-      console.log(ledger)
-
-      // write files
-      console.log('wrting files', ledgerFile, creditsFile)
-      fs.writeFileSync(ledgerFile, JSON.stringify(ledger, null, 2))
-      fs.writeFileSync(creditsFile, JSON.stringify(credits, null, 2))
-    } else {
-      console.log('no funds')
-      ctx.reply('no funds')
-    }
-
-  }  
 
   // deposit
   if (message[0].toLocaleLowerCase() === 'deposit') {
@@ -571,11 +509,18 @@ sweep <txid:vout>
     commands.wallet(ctx, wallet)
   }
 
+  // mark
+  if (message[0].toLocaleLowerCase() === 'mark') {
+    console.log('mark', message)
+    commands.mark(ctx, from, usernames, message, ledger, credits, ledgerFile, creditsFile)
+  }  
+
   // help
   if (message[0].toLocaleLowerCase() === 'help') {
     console.log('help', message)
     commands.help(ctx)
   }
+
 
 })
 
